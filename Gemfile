f3392225827e7dbac2b6ee52908ed892ce79542f
sven liu <sven@svendeMacBook-Pro.local>
2016-08-04 23:20:37 +0800

    add spec

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'https://ruby.taobao.org'
 
+gem 'rake'
 # Specify your gem's dependencies in mozilla-only.gemspec
 gemspec
--- spec/mozilla/only_spec.rb
+++ spec/mozilla/only_spec.rb
@@ -8,4 +8,24 @@ describe Mozilla::Only do
   it 'does something useful' do
     expect(true).to eq(true)
   end
+
+  describe "when firefox" do
+    it "should have the response code 400" do
+      env = { "HTTP_USER_AGENT" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:48.0) Gecko/20100101 Firefox/48.0 " }
+      header = {}
+      client = Mozilla::Only::Client.new(proc { [200, header, ["This is body."]] })
+      expect(client.call(env)).to eq [400, header, ["Bad Request"]]
+    end
+  end
+
+  describe "when google chorme" do
+    it "should have the response code 200" do
+      env = { "HTTP_USER_AGENT" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36" }
+      code = 200
+      header = {}
+      body = ["This is body."]
+      client = Mozilla::Only::Client.new(proc { [code, header, body]})
+      expect(client.call(env)).to eq [code, header, body]
+    end
+  end
 end
